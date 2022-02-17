3ce46c7940b1e0a4a8cb4c7457493067fe7929e4
reduan <reduan15-11413@diu.edu.bd>
2022-02-17 17:50:04 +0600

    set arror

--- js/app.js
+++ js/app.js
@@ -11,13 +11,10 @@ function expenses() {
 
   if (isNaN(food) || food < 0) {
     document.getElementById("foodFeedback").classList.add("active");
-    totalExpenses = rent + clothes;
   } else if (isNaN(rent) || rent < 0) {
     document.getElementById("rentFeedback").classList.add("active");
-    totalExpenses = food + clothes;
   } else if (isNaN(clothes) || clothes < 0) {
     document.getElementById("clothesFeedback").classList.add("active");
-    totalExpenses = food + rent;
   } else {
     totalExpenses = food + rent + clothes;
     document.getElementById("foodFeedback").classList.remove("active");
@@ -35,18 +32,23 @@ function expenses() {
 document.getElementById("calculateBtn").addEventListener("click", function () {
   const incomeInput = document.getElementById("inputIncome").value;
   const income = parseInt(incomeInput);
+  const totalExpenses = expenses();
 
   let balance = 0;
   if (isNaN(income) || income < 0) {
     document.getElementById("incomeFeedback").classList.add("active");
+    document.getElementById("incomeFeedback2").classList.remove("active");
   } else {
-    const totalExpenses = expenses();
     if (income < totalExpenses) {
       balance = income - totalExpenses;
       document.getElementById("incomeFeedback2").classList.add("active");
+      document.getElementById("incomeFeedback").classList.remove("active");
+      document.getElementById("totalBalance").style.color = "red";
     } else {
       balance = income - totalExpenses;
       document.getElementById("incomeFeedback").classList.remove("active");
+      document.getElementById("incomeFeedback2").classList.remove("active");
+      document.getElementById("totalBalance").style.color = "rgb(68, 54, 54)";
     }
   }
 
@@ -61,23 +63,46 @@ function percentageCalculate() {
   const income = parseInt(incomeInput);
   const saveInput = document.getElementById("inputSave").value;
   const savePercentage = parseInt(saveInput);
-  const saveBalance = (income * savePercentage) / 100;
+  let saveBalance = 0;
+  if (isNaN(savePercentage) || savePercentage < 0 || isNaN(income)) {
+    document.getElementById("saveFeedback").classList.add("active");
+    document.getElementById("saveFeedback2").classList.remove("active");
+  } else {
+    if (savePercentage < 100) {
+      saveBalance = (income * savePercentage) / 100;
+      document.getElementById("saveFeedback").classList.remove("active");
+      document.getElementById("saveFeedback2").classList.remove("active");
+    } else {
+      document.getElementById("saveFeedback2").classList.add("active");
+      document.getElementById("saveFeedback").classList.remove("active");
+    }
+  }
 
   //display save balance
   const saveBalanceDisplay = document.getElementById("saveBalance");
   saveBalanceDisplay.innerHTML = saveBalance;
   return saveBalance;
 }
-//calculate save balance
+//calculate remaining balance
 document.getElementById("saveBtn").addEventListener("click", function () {
   const totalBalanceText = document.getElementById("totalBalance").innerText;
   const totalBalance = parseInt(totalBalanceText);
   const saveAmmountText = percentageCalculate();
   const saveAmmount = parseInt(saveAmmountText);
-
+  let remainingBalance = 0;
   //calculate remaining balance
-  const remainingBalance = totalBalance - saveAmmount;
-
+  if(isNaN(totalBalance) == false) {
+    if(saveAmmount < totalBalance) {
+        remainingBalance = totalBalance - saveAmmount;
+        document.getElementById("remainingBalance").style.color = "rgb(68, 54, 54)";
+        document.getElementById("remainingFeedback").classList.remove("active");
+    }else{
+        remainingBalance = totalBalance - saveAmmount;  
+        document.getElementById("remainingBalance").style.color = "red";
+        document.getElementById("remainingFeedback").classList.add("active");
+    }
+  }
+  
   //display remaining balance
   const displayRemainingBalance = document.getElementById("remainingBalance");
   displayRemainingBalance.innerHTML = remainingBalance;
